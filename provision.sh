ca215140d8ed9793d99197718cf9e0fb16625cb6
Jake Bladt <jake@jakebladt.com>
2016-10-25 20:57:17 -0400

    use vagrant build-out from node sandbox

---
+++ provision.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+
+cd ~
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
