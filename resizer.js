880e768b9f4dfbc931236e5df19a8052b9e38cf0
Jake Bladt <jake@jakebladt.com>
2016-12-07 20:48:07 -0500

    forEach->for

--- resizer.js
+++ resizer.js
@@ -19,7 +19,7 @@ var pathParams = {
 
 s3.listObjects(pathParams, function(err, data) {
   if(err) console.log('ERR:', err);
-  data.forEach(function(item, index) {
-    console.log(item.Key);
-  });
+  for(i = 0; i < data.length; i++) {
+    console.log(data[i].Key);
+  }
 });
