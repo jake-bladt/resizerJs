425c799f8af6cb03dd576ecaad85ca23b24adc67
Jake Bladt <jake@jakebladt.com>
2016-12-07 20:25:58 -0500

    +tarDir

--- resizer.js
+++ resizer.js
@@ -9,5 +9,6 @@ aws.config.update({
 // Hard-code region for now
 aws.config.update({region: 'us-west-2'});
 var s3 = new aws.S3();
-var srcDir = process.argv[1]
-console.log(srcDir);
+var srcDir = process.argv[2],
+    tarDir = process.argv[3];
+console.log(srcDir, " -> ", tarDir);
