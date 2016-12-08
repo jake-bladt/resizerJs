ab33ef8018081a00d7d84a97e96ca95dc2e5b71b
Jake Bladt <jake@jakebladt.com>
2016-12-07 20:23:33 -0500

    =->:

--- secrets.template.js
+++ secrets.template.js
@@ -1,4 +1,4 @@
 module.exports = {
-  AWSAccessKeyId = 'YOUR_AWS_ACCESS_KEY_ID',
-  AWSSecretAccessKey = 'YOUR_AWS_SECRET_ACCESS_KEY'  
+  AWSAccessKeyId:'YOUR_AWS_ACCESS_KEY_ID',
+  AWSSecretAccessKey: 'YOUR_AWS_SECRET_ACCESS_KEY'  
 };
